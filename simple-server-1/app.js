e549ae6397d47a295ac473b87b61587546ef0de1
Brandon Johnson <brandonjohnson@Brandons-MacBook-Pro.local>
2017-11-27 13:24:16 -0700

    starting over

--- simple-server-1/app.js
+++
@@ -1,7 +0,0 @@
-const cohorts = require('./data/cohorts')
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.json('/'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
