69b71b5395b9c1d6a59ce19e21139e62769c4f6c
Brandon Johnson <brandonjohnson@Brandons-MacBook-Pro.local>
2017-11-27 18:17:27 -0700

    good through firs app.get

--- simple-server-1/app.js
+++ simple-server-1/app.js
@@ -1,6 +1,36 @@
+const cohorts = require('./data/cohorts')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.json(cohorts))
+
+function findById(cohorts, id) {
+  for (let i = 0; i < cohorts.length; i++) {
+    for (var j = 0; j < cohorts..length; i++) {
+      array[i]
+    }
+    if (cohorts[i].id == id) {
+      return cohorts[i];
+      console.log(cohortData, cohorts[i])
+    }
+  }
+  return null;
+}
+
+app.get("/", function(request, response) {
+  response.json({
+    data: cohorts
+  });
+});
+
+app.get("/:id", function(request, response) {
+  let specificCohort = findById("id");
+  response.json({
+    data: specificCohort
+  })
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
